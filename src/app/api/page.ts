04f07f32c32fbbdee573f6512dd6cf3c0b550bc1
christianwilkins <chrisjw12345@gmail.com>
2024-02-18 22:47:59 -0500

    Removed api key :)

--- src/app/api/page.ts
+++ src/app/api/page.ts
@@ -1,8 +1,8 @@
 import { OpenAI } from "openai";
 
-const openaiKey = process.env.REACT_APP_OPENAI_API_KEY;
+const openaiKey = process.env.OPENAI_API_KEY;
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: openaiKey,
   /* apiKey: "", */
   dangerouslyAllowBrowser: true, // lmao
 });
