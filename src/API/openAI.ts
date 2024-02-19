9b80132cd5c3b95dde21a1b8733e51c7493f947d
christianwilkins <chrisjw12345@gmail.com>
2024-02-18 22:45:50 -0500

    Removed key problem?

--- src/API/openAI.ts
+++ src/API/openAI.ts
@@ -1,9 +1,1 @@
-import OpenAI from "openai";
-
-const openaiKey = process.env.REACT_APP_OPENAI_API_KEY;
-const openai = new OpenAI({
-  apiKey: "key",
-  /* apiKey: "", */
-  dangerouslyAllowBrowser: true, // lmao
-});
 
