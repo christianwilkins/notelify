ddc08750efed187f5d085c59a39a4fcddadc7171
Devansh Tayal <devliveforever@gmail.com>
2024-03-15 17:42:24 -0400

    removed page.ts

--- src/app/api/page.ts
+++
@@ -1,8 +0,0 @@
-import { OpenAI } from "openai";
-
-const openaiKey = process.env.OPENAI_API_KEY;
-const openai = new OpenAI({
-  apiKey: openaiKey,
-  /* apiKey: "", */
-  dangerouslyAllowBrowser: true, // lmao
-});
