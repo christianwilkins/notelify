20fda1f55c4a709f90a3eddb5703f740a9f0cbf8
MAlshaik <alshaik8@msu.edu>
2024-03-25 14:44:52 -0400

    google authetication implimented and login functionality is moved to client-side

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,8 @@
-import { login, signup } from './actions'
+"use client"
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useState } from 'react'
+import {handleSignIn, handleSignUp, loginWithGoogle} from './utils'
+
 import {
   Card,
   CardContent,
@@ -13,10 +17,14 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
 export default function LoginPage() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
 
     <div className="flex items-center justify-center  h-full">
-    <form>
+    <div className="flex flex-col gap-3">
       <Card className="mx-auto max-w-sm">
       <CardHeader className="space-y-1">
         <CardTitle className="text-2xl font-bold">Login</CardTitle>
@@ -26,20 +34,32 @@ export default function LoginPage() {
         <div className="space-y-4">
           <div className="space-y-2">
             <Label htmlFor="email">Email</Label>
-            <Input id="email" name="email" type="email" placeholder="exmp@example.com" required />
+            <Input 
+                id="email" 
+                name="email" 
+                type="email" 
+                placeholder="exmp@example.com" 
+                onChange={(e) => setEmail(e.target.value)}
+                required />
           </div>
          <div className="space-y-2">   
             <Label htmlFor="password">Password:</Label>
-            <Input id="password" name="password" type="password" required />
+            <Input 
+                id="password" 
+                name="password" 
+                type="password" 
+                onChange={(e) => setPassword(e.target.value)}
+                required />
          </div>
       <div className="flex gap-x-2">
-      <Button formAction={login}>Log in</Button>
-      <Button formAction={signup}>Sign up</Button>
+      <Button onClick={handleSignIn}>Log in</Button>
+      <Button onClick={handleSignUp}>Sign up</Button>
       </div>
       </div>
       </CardContent>
       </Card>
-    </form>
+    <Button onClick={loginWithGoogle} >Google</Button>
+    </div>
     </div>
   )
 }
